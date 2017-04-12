85797ac644cc6d91ce516994efeba8adbe16c3a1
Gan Huang <344031924@qq.com>
2017-04-12 22:02:32 +0800

    testing update

--- LW_GUI/GLW_update.m
+++ LW_GUI/GLW_update.m
@@ -1,4 +1,5 @@
-function GLW_update(~)
+function is_updated=GLW_update()
+is_updated=0;
 scrsz = get(0,'ScreenSize');
 pos=[scrsz(3)/2,scrsz(4)/2,250,100];
 fig=figure('Position',pos,'color',[0.93,0.93,0.93],...
@@ -9,58 +10,67 @@ update_btn=uicontrol('style','pushbutton','callback',@updateing,...
     'String','update','position',[25,10,200,50],'enable','off');
 update_txt=uicontrol('style','text','backgroundcolor',[0.93,0.93,0.93],...
     'String','checking...','position',[5,70,240,20]);
-pause(0.001);
+
 url='https://raw.githubusercontent.com/NOCIONS/letswave7/master/resources/version.txt';
 try
     lw_version = str2num(urlread(url));
-    handles.version_checkked=1;
     temp=load('version.txt');
     if temp<lw_version
-        set(update_txt,'string','There is new version for updating.','enable','on');
+        set(update_txt,'string','There is new version for updating.');
+        set(update_btn,'enable','on');
     else
         set(update_txt,'string','The currest version is already the latest.');
     end
 catch
     set(update_txt,'string','Unable to access the website.');
 end
-end
+uiwait(fig);
 
-function updateing()
-disp('123');
-% set(handles.current_text,'String','Downloading from server...');
-% drawnow;
-% fullURL=['https://github.com/NOCIONS/letswave6/archive/master.zip'];
-% st=which('letswave6.m');
-% [p n e]=fileparts(st);
-% filename=[p filesep 'letswave6.zip'];
-% try
-%     urlwrite(fullURL,filename);
-% catch
-%     disp('Failed to download from server.');
-% end;
-% set(handles.current_text,'String','File downloaded. Updating Letswave...');
-% filenames=unzip(filename,p);
-% filenames2=filenames;
-% st2=[filesep 'letswave6-master'];
-% st2_length=length(st2);
-% for filepos=1:length(filenames);
-%     st=filenames{filepos};
-%     a=strfind(st,st2);
-%     st(a(end):a(end)+st2_length-1)=[];
-%     filenames2{filepos}=st;
-% end;
-% for filepos=1:length(filenames);
-%     try
-%         [SUCCESS,MESSAGE,MESSAGEID]=copyfile(filenames{filepos},filenames2{filepos});
-%         if SUCCESS==0;
-%             disp(['Could not update : ' filenames2{filepos}]);
-%         end;
-%     catch
-%         disp(['Could not update : ' filenames2{filepos}]);
-%         disp(MESSAGEID);
-%     end;
-% end;
-% rmdir([p filesep 'letswave6-master'],'s');
-% delete([p filesep 'letswave6.zip']);
-% set(handles.current_text,'String','Finished installing.');
+
+    function updateing(~,~)
+        set(update_txt,'String','Downloading from server...');
+        set(update_btn,'enable','off');
+        pause(0.001);
+        fullURL='https://github.com/NOCIONS/letswave7/archive/master.zip';
+        st=which('letswave7.m');
+        p=fileparts(st);
+        filename=fullfile(p,'letswave7.zip');
+        try
+            urlwrite(fullURL,filename);
+        catch
+            set(update_txt,'String','Failed to download from server.');
+            set(update_btn,'enable','on');
+            pause(0.001);
+        end
+        set(update_txt,'String','File downloaded. Updating Letswave...');
+        pause(0.001);
+        filenames=unzip(filename,p);
+        filenames2=filenames;
+        st2=[filesep 'letswave7-master'];
+        st2_length=length(st2);
+        for filepos=1:length(filenames)
+            st=filenames{filepos};
+            a=strfind(st,st2);
+            st(a(end):a(end)+st2_length-1)=[];
+            filenames2{filepos}=st;
+        end
+        for filepos=1:length(filenames)
+            try
+                [SUCCESS,~,MESSAGEID]=copyfile(filenames{filepos},filenames2{filepos});
+                if SUCCESS==0
+                    if ~strcmp(filenames2{filepos},[p,filesep])
+                    disp(['Could not update : ' filenames2{filepos}]);
+                    end
+                end
+            catch
+                disp(['Could not update : ' filenames2{filepos}]);
+                disp(MESSAGEID);
+            end
+        end
+        rmdir([p filesep 'letswave7-master'],'s');
+        delete([p filesep 'letswave7.zip']);
+        set(update_txt,'String','Finished installing. Letswave will restart.');
+        pause(0.5);
+        is_updated=1;
+    end
 end
--- LW_manager.m
+++ LW_manager.m
@@ -164,6 +164,7 @@ Manager_Init();
         %update_handles
         update_handles();
         %% init timer
+        pause(0.01);
         handles.timer = timer('BusyMode','drop','ExecutionMode','fixedRate','TimerFcn',{@(obj,events)on_Timer()});
         start(handles.timer);
         set(handles.fig,'handlevisibility','off');
@@ -308,6 +309,14 @@ Manager_Init();
     function menu_callback(fun_name)
         %executes on menu_callback
         %fun_name = name of function associated with menu callback
+        if strcmp(fun_name,'GLW_update')
+            if GLW_update()
+                fig_Close();
+                letswave7;
+            end
+            return;
+        end
+        
         if ~isempty(strfind(fun_name,'FLW_import_'))
             %if fun_name is  FLW_import
             %execute the function with handles.fig
