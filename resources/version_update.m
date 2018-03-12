a293c3afc3c5fdba453ab32e44f792710ef4a29b
Gan Huang <344031924@qq.com>
2018-03-12 10:16:38 +0800

    daily update 2018.03.12
    missing a semicolon

--- resources/version_update.m
+++
@@ -1,8 +0,0 @@
-clc;clear;
-[pathstr] = fileparts(mfilename('fullpath'));
-file_name=fullfile(pathstr,'version.txt');
-t=datetime('now','format','yyyyMMddHHmmss','TimeZone','Etc/UTC');
-lw_version=str2num(char(t));
-fileID = fopen(file_name,'w');
-fwrite(fileID,char(t));
-fclose(fileID);
